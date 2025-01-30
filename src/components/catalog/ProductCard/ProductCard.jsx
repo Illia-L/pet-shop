fcfe7d9fdb501849431ffda72ef700245afdc83b
Illia Liashko <2275040@gmail.com>
2025-01-30 09:03:04 +0200

    add product page

--- src/components/catalog/ProductCard/ProductCard.jsx
+++ src/components/catalog/ProductCard/ProductCard.jsx
@@ -1,19 +1,22 @@
+import { NavLink } from 'react-router-dom';
 import styles from './ProductCard.module.css';
 
 function ProductCard({product}) {
   return (
     <li className={styles.card}>
-      <div className={styles.imageRelativeBox}>
-        <div className={styles.imageAbsoluteBox}>
-          <img
-            src={'/pet-shop' + product.image}
-            alt={product.title}
-            className={styles.image}
-          />
+      <NavLink to={`products/${product.id}`} className={styles.link}>
+        <div className={styles.imageRelativeBox}>
+          <div className={styles.imageAbsoluteBox}>
+            <img
+              src={'/pet-shop' + product.image}
+              alt={product.title}
+              className={styles.image}
+            />
+          </div>
         </div>
-      </div>
-      <h3 className={styles.title}>{product.title}</h3>
-      <p className={styles.price}>{product.price}</p>
+        <h3 className={styles.title}>{product.title}</h3>
+        <p className={styles.price}>{product.price}</p>
+      </NavLink>
     </li>
   );
 }
