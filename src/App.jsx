371507cf7a38764f1fc4920bec9665dd4f92ed42
Illia Liashko <2275040@gmail.com>
2025-02-02 23:13:58 +0200

    refactor cabinet to redux

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,18 @@ import ProductDetails from './components/ProductDetails/ProductDetails';
 import BasketPage from './pages/BasketPage/BasketPage';
 import Cabinet from './components/cabinet/Cabinet/Cabinet';
 import Signup from './components/cabinet/Signup/Signup';
+import Login from './components/cabinet/Login/Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchUser } from './redux/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, []);
+
   return (
     <Layout>
       <Routes>
@@ -30,18 +40,36 @@ function App() {
         />
         <Route
           path='products/:id'
-          element={<ProductDetails/>}
+          element={<ProductDetails />}
         />
         <Route
           path='cabinet'
-          element={<Cabinet/>}
+          element={<Cabinet />}
         >
-          <Route path='login' element={<p>Login</p>}/>
-          <Route path='logout' element={<p>Logout</p>}/>
-          <Route path='signup' element={<Signup/>}/>
-          <Route path='orders' element={<p>Orders</p>}/>
-          <Route path='profile' element={<p>Profile</p>}/>
-          <Route path='edit-profile' element={<p>Edit Profile</p>}/>
+          <Route
+            path='login'
+            element={<Login />}
+          />
+          <Route
+            path='logout'
+            element={<p>Logout</p>}
+          />
+          <Route
+            path='signup'
+            element={<Signup />}
+          />
+          <Route
+            path='orders'
+            element={<p>Orders</p>}
+          />
+          <Route
+            path='profile'
+            element={<p>Profile</p>}
+          />
+          <Route
+            path='edit-profile'
+            element={<p>Edit Profile</p>}
+          />
         </Route>
       </Routes>
     </Layout>
