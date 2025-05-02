ee37053a9861ddcd94e8ff19a67f24748c41892b
Illia Liashko <2275040@gmail.com>
2025-05-02 14:26:21 +0300

    fix productDetailForm

--- src/components/ProductDetailsForm/ProductDetailsForm.jsx
+++ src/components/ProductDetailsForm/ProductDetailsForm.jsx
@@ -1,37 +1,43 @@
-import css from "./ProductDetailsForm.module.css";
+import css from './ProductDetailsForm.module.css';
 import {
   addItemToCart,
   incrementQuantity,
   decrementQuantity,
-} from "../../redux/productsSlice";
-import { useDispatch, useSelector } from "react-redux";
-import clsx from "clsx";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { DiAndroid } from "react-icons/di";
+} from '../../redux/productsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { DiAndroid } from 'react-icons/di';
 
 const MAX_QUANTITY = 50;
 
 function ProductDetailsForm({ product }) {
   const [quantity, setQuantity] = useState(1);
   const dispatch = useDispatch();
-  const cart = useSelector((state) => state.products.items);
+  const cart = useSelector(state => state.products.items);
 
-  const cartProduct = cart.find((p) => p.id === product.id);
+  const cartProduct = cart.find(p => p.id === product.id);
   const cartQuantity = cartProduct?.quantity;
-  const buttonText = cartProduct ? "У кошику" : "Купити";
+  const buttonText = cartProduct ? 'У кошику' : 'Купити';
   const buttonClassList = [css.toCart];
+
+  useEffect(() => {
+    if (cartProduct) setQuantity(cartQuantity);
+  }, []);
 
   if (cartProduct) buttonClassList.push(css.reversedColors);
 
-  if (cartProduct && quantity !== cartQuantity) setQuantity(cartQuantity);
-
   function increaseQuantity() {
-    setQuantity((q) => q + 1);
+    if (!cartProduct) dispatch(addItemToCart({ product, quantity: 1 }));
+
+    setQuantity(q => q + 1);
+    dispatch(incrementQuantity(product.id));
   }
 
   function decreaseQuantity() {
-    setQuantity((q) => q - 1);
+    setQuantity(q => q - 1);
+    dispatch(decrementQuantity(product.id));
   }
 
   function handleSubmit(e) {
@@ -40,12 +46,15 @@ function ProductDetailsForm({ product }) {
   }
 
   return (
-    <form className={css.order} onSubmit={handleSubmit}>
+    <form
+      className={css.order}
+      onSubmit={handleSubmit}
+    >
       <div className={css.sumBox}>
         <div className={css.quantityBox}>
           <button
             className={css.quantityButton}
-            type="button"
+            type='button'
             disabled={quantity <= 1}
             onClick={decreaseQuantity}
           >
@@ -56,7 +65,7 @@ function ProductDetailsForm({ product }) {
 
           <button
             className={css.quantityButton}
-            type="button"
+            type='button'
             disabled={quantity >= MAX_QUANTITY}
             onClick={increaseQuantity}
           >
@@ -69,7 +78,7 @@ function ProductDetailsForm({ product }) {
 
       <button
         className={clsx(buttonClassList)}
-        type="submit"
+        type='submit'
         disabled={!!cartProduct}
       >
         {buttonText}
